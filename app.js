c54ec509818e0b09753dde31f64de65711874dc3
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2022-12-13 16:17:23 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
-let phoneCost=119.95
-let phoneNumb=30
-let taxRate=5.9975
-let phoneCostWithTaxRare=phoneCost+(phoneCost*taxRate/100)
-let total=phoneCostWithTaxRare*phoneNumb
-console.log(total)
+let ChineseZodiac=prompt("What year of Chinese Zodiac are you?")
+let numberInput=ChineseZodiac
+let numberResult=Number(ChineseZodiac) 
+
+
+
+
+
+if  (numberInput <= 0) {
+    console.log("Rat");
+    
+} else if (numberInput <= 1) {
+    console.log("Ox");
+    
+}else if (numberInput <= 2)  {
+    console.log("Tiger");
+    
+} else if (numberInput <= 3) {
+    console.log("Rabbit");
+    
+}else if (numberInput <= 4) {
+    console.log("Dragon");
+    
+}else if (numberInput <= 5) {
+    console.log("Snake")
+}else if (numberInput <= 6) {
+    console.log("Horse")
+
+}else if (numberInput <= 7) {
+    console.log("Goat")
+    
+}else if (numberInput <= 8) {
+    console.log("Monkey")
+    
+}else if (numberInput <= 9) {
+    console.log("Rooster")
+
+    
+}else if (numberInput <= 10) {
+    console.log("Dog")
+    
+}else if (numberInput <= 11) {
+    console.log("Pig")
+}else  {
+
+    console.log("Try more times")
+
+}
+    
+
+    
+    
+
+
