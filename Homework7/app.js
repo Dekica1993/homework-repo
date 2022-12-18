8ea2bdf6b268c05a037a851350e43b3b1d043666
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2022-12-18 20:54:20 +0100

    Add files via upload

---
+++ Homework7/app.js
@@ -0,0 +1,19 @@
+console.log("it works")
+
+listOfArray=[2,4,6,8,10]
+function returnTheSum(listOfArray){
+    console.log(listOfArray)
+
+}
+console.log(listOfArray)
+console.log(listOfArray[0]+listOfArray[1]+listOfArray[2]+listOfArray[3]+listOfArray[4])
+
+function validateNumber(listOfArray){
+    console.log(listOfArray)
+}
+validateNumber(listOfArray[0])
+validateNumber(listOfArray[1])
+validateNumber(listOfArray[2])
+validateNumber(listOfArray[3])
+validateNumber(listOfArray[4])
+validateNumber(listOfArray(4))
