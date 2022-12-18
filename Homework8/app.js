45ca7de8f2cd8aad914010b8299bf49c824f061d
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2022-12-18 21:17:42 +0100

    Add files via upload

---
+++ Homework8/app.js
@@ -0,0 +1,8 @@
+console.log("it works")
+
+
+let textArray=["Hello","there","students","of","SEDC","!"]
+function createOneBigString(textArray){
+    console.log(textArray)
+}
+console.log(textArray[0],textArray[1],textArray[2],textArray[3],textArray[4],textArray[5])
