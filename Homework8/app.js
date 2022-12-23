c09541669decfe62690b5b20262c60c2d743511b
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2022-12-23 17:54:58 +0100

    Delete Homework8 directory

--- Homework8/app.js
+++
@@ -1,8 +0,0 @@
-console.log("it works")
-
-
-let textArray=["Hello","there","students","of","SEDC","!"]
-function createOneBigString(textArray){
-    console.log(textArray)
-}
-console.log(textArray[0],textArray[1],textArray[2],textArray[3],textArray[4],textArray[5])
