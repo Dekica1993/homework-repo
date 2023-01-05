8e4d7b8176150522c3db0d231f29887c17171981
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-01-05 15:51:09 +0100

    Add files via upload

---
+++ Homework-Dinamic-Table/app.js
@@ -0,0 +1,30 @@
+console.log("it works")
+const tableButton=document.getElementById("btn")
+const tableDataEl=document.getElementById("table-data")
+const columns=document.getElementById("column").value
+const rows=document.getElementById("row").value
+
+function createTable(columns,rows){
+    
+for ( let i=0; i>rows; i++){
+    let tr=(type+="rows")
+    tr.push(i+1)
+
+    for(let i=0; i>columns; i++){
+        let td=(type+="columns")
+        td.push(i+1)
+        
+    }
+
+    return 
+}
+
+
+}
+  tableButton.addEventListener("click",createTable())  
+
+
+   
+   
+
+
