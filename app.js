0af0fcb6b8d937e43f860132d2e74c50e753b8a9
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-01-31 17:17:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,97 @@
-console.log("It works")
+// const nameEl=document.querySelector("name")
+// const cardEl=document.querySelector("fake-card")
 
-function parametarOne(object){
-    let result=object
-    console.log(result)
-    return result
+// const fetchName=("https://fakestoreapi.com/products")
+
+
+// function fetchProducts(){
+//     fetch(fetchName)
+//     .then (function(res){
+//         return res.json()
+//     })
+//     .then(function(data){
+//         renderProducts(nameEl,data)
+        
+           
+//     })
+  
+   
+
+// }
+
+// fetchProducts()
+// function renderProducts(nameEl,cardEl){
+//     let productsHTML=""
+//     for(let products of nameEl){
+//         productsHTML.innerHTML+= `<div class="fake-card"></div>
+//         <h3>${products.id} ${products.title}</h3>`
+        
+//     }    
+//     nameEl.innerHTML=productsHTML 
+//     }
+
+// const sayHello=()=>{
+//     console.log("hello from the arrow funcion")
+// }
+
+// sayHello()
+
+// const prinMoney=money=>`You have a total of ${money}$`
+
+// console.log(prinMoney(10000))
+
+
+// function makeDigitsNumb(numOne){
+//     return numOne
+// }
+// console.log(makeDigitsNumb(30))
+
+// function getNumberEven(operator,numOne,numTwo){
+//     if(operator==="even"){
+//       return  (numOne,numTwo)
+//     }
+//     if(operator==="odd"){
+//         return (numOne,numTwo)
+//     }
+// }
+// console.log(getNumberEven("even",2,4))
+// console.log(getNumberEven("odd",1,3))
+
+// function returnNumber(operator){
+//     if(operator>0){
+//         return ("positive")
+//     }
+//     if(operator<0){
+//         return ("negative")
+//     }
     
+
+
+// }
+// console.log(returnNumber(4))
+// console.log(returnNumber(-8))
+
+// function getNumberStats(){
+//     makeDigitsNumb()
+//     getNumberEven()
+//     returnNumber()  
+// }
+// console.log(getNumberStats(-25))
+
+
+const RAW_GITHUB="https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/students_v2.json"
+
+fetch("https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/students_v2.json")
+.then(res=>res.json())
+.then(data=>app(data))
+const app=data=>{
+    const studentFilter=data.filter(student=>student.averageGrade>=3)
+    console.log(studentFilter)
+    const studentFemale=data.map(student=>{
+       console.log(`${data.firstName} ${} `)
+    })
+    console.log(studentFemale)
 }
-parametarOne(null)
 
 
-function parametarTwo(boolean){
-    let result1=boolean
-    console.log(result1)
-    return result1
-}
-parametarTwo(false)
 
-function parametarTree(number){
-    let result3=number
-    console.log(result3)
-    return result3
-
-}
-parametarTree(29)
-
-function parametarFour(string){
-let result4=string
-console.log(result4)
-return result4
-}
-parametarFour("Dejan")
-
-function parametarFive(undefined){
-    let result5=undefined
-    console.log(result5)
-    return result5
-}
-parametarFive()
