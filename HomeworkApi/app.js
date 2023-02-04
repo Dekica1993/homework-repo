b822fa0086bbdc2187be08c6c73501fdba009707
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-02-04 11:25:41 +0100

    Add files via upload

---
+++ HomeworkApi/app.js
@@ -0,0 +1,30 @@
+console.log("works")
+
+planetListEl=document.querySelector(".plenet-list")
+planetCardEl=document.querySelector(".planet-card")
+clickBtn=document.querySelector("#btn")
+
+console.log(planetListEl,planetCardEl,clickBtn)
+
+
+const fetchPlanets=()=>{
+    fetch("https://swapi.dev/api/planets/?page=1")
+    .then(res=>res.json())
+    .then(planets=>renderPlanetList(planets))
+    
+}   
+const renderPlanetList=planets=>{
+    const firstShowData=planets.map(planet=>`${planet.name} ${planet.population} ${planet.climate} ${planet.gravity}`)
+    console.log(firstShowData)
+}   
+
+
+fetchPlanets()
+
+
+
+
+
+
+   
+        
