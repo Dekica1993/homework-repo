f474bc50e0c9a6ab7c7d5066cecee07ea296e568
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-02-12 18:54:12 +0100

    Add files via upload

---
+++ AdvanceJavaScript-Homeworks/homework-AsyncAwait/app.js
@@ -0,0 +1,50 @@
+console.log("works")
+
+
+const fetchStudentsPromise=()=>{
+    fetch("https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/students_v2.json")
+    .then(res=>res.json())
+    .then(students=>generateSolutions(students))
+}
+const generateSolutions=students=>{
+    
+    students.innerHTML=`<li>${students.firstName}</li>`
+    const allStudentsCombined=students.reduce((acc,student)=>{
+        return acc+student.age /100
+        
+    },0)
+    console.log(allStudentsCombined)
+    console.log(students.reduce((acc,student)=>{
+        return acc+student.averageGrade /100
+    },0))
+    console.log(students.reduce((acc,student)=>{
+     return acc+student.age >60
+    },0))
+    console.log(students.reduce((acc,student)=>{
+        return acc+student.age <30
+       },0))
+       console.log(students.filter(student=>student.firstName && 
+        student.city && 
+        student.age >30 && 
+        student.averageGrade>=4))   
+        const name=students.find(student =>student.firstName==="Artuh")
+        console.log(name)
+        console.log(students.find(student=>student.age<=12))
+        console.log(students.find(student=>student.age>=42))
+        console.log(students.filter(student=>student.firstName && student.lastName[8]))
+        console.log(students.filter(student=>student.averageGrade[0,10]>=5))
+        console.log(students.filter(student=>student.age>18 && student.averageGrade>=1))
+        
+        
+}
+
+
+
+
+
+
+
+
+
+
+fetchStudentsPromise()
