121586f0c672ba64dbeebcf07c75d95332681ab6
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-02-12 18:56:52 +0100

    Delete BasicJavaScript directory

--- BasicJavaScript/homework-Array/Homework1-array/app.js
+++
@@ -1,9 +0,0 @@
-console.log("it works")
-
-
-let currentForm=['name','mood','activity']
-function tellStory(name,mood,activity){
-    return `This is ${name}. ${name} is nice person, he is ${mood} because he is having the time of his life.Today he is in a good mood.
-    He was ${activity} all day`
-}
- console.log(tellStory("Dejan", "happy","running"))
