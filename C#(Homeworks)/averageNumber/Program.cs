926779bc6dd63bf55b2c78723f768584e4a69f06
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-03-10 22:43:18 +0100

    Add files via upload

---
+++ C#(Homeworks)/averageNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please input four numbers!");
+double numberOne = Convert.ToInt32(Console.ReadLine());
+double numberTwo = Convert.ToInt32(Console.ReadLine());
+double numberThree = Convert.ToInt32(Console.ReadLine());
+double numberFour = Convert.ToInt32(Console.ReadLine());
+double result = ((numberOne + numberTwo + numberThree + numberFour) / 4); 
+Console.WriteLine("The average of:"  + numberOne +  ","
+    + numberTwo + "," + numberThree+ "," + numberFour +  " " +
+    " is:" + result);
